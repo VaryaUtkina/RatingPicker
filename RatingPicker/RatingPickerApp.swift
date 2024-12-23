43a9148dcee1759109d596536d2518a80c11b92c
Varvara Utkina <8z9spfh2p5@privaterelay.appleid.com>
2024-12-23 16:29:00 +0300

    Initial Commit

---
+++ RatingPicker/RatingPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingPickerApp.swift
+//  RatingPicker
+//
+//  Created by Варвара Уткина on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct RatingPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
